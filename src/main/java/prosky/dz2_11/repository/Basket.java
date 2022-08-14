27443b8de4c4363880c9be1abd1dc0c32270412e
EduardGeras <103679703+EduardGeras@users.noreply.github.com>
2022-08-15 01:06:02 +0500

    Домашка 2.11 доработка

--- src/main/java/prosky/dz2_11/repository/Basket.java
+++ src/main/java/prosky/dz2_11/repository/Basket.java
@@ -1,12 +1,12 @@
 package prosky.dz2_11.repository;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.SessionScope;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Repository
+@Component
 @SessionScope
 public class Basket {
     private final List<Integer> productBucket = new ArrayList<>();
